879942bae50d1ac62433ec49989ba0599b52dedb
Anselm Hook <anselm@gmail.com>
2022-12-19 17:11:13 -0800

    touchups to basic sdl

--- sys/apps/basic/basic_sdl.js
+++ sys/apps/basic/basic_sdl.js
@@ -1,5 +1,7 @@
 
-// defining a basic 3d scene
+////////////////////////////////////////////////////////////////////////////////////////////////
+// a 3d scene - orbital has a 'throw data over the wall' philosophy of defining 3d scenes
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 let mycamera = {
 	uuid: "/myusername/apps/basic001/camera1",
@@ -84,6 +86,11 @@ let myscene = {
 	children: [ mycamera, mylights, mything1, myux ],
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+// an application - orbital allows you to declare and wire components up in a manifest
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 let myapp = [
 
 	///////////////////////////////////////////////////////////////////////////////////////
@@ -116,7 +123,7 @@ let myapp = [
 	// setup networking
 	///////////////////////////////////////////////////////////////////////////////////////
 
-	// make a network and hack in some filtering on traffic
+	// make a network and tack in some filtering on traffic - filter can be a function
 	{
 		urn: "*:/sys/services/netclient",
 		uuid: "/myusername/apps/basic001/mynet",
@@ -139,7 +146,7 @@ let myapp = [
 	},
 
 	///////////////////////////////////////////////////////////////////////////////////////
-	// add an handler for us to move around a puppet with
+	// add an handler for us to move around a puppet with - each player gets a puppet
 	///////////////////////////////////////////////////////////////////////////////////////
 
 	// make a handler for our avatar
@@ -151,21 +158,21 @@ let myapp = [
 		data: myavatar,
 	},
 
-];
 
-let unused = [
+	///////////////////////////////////////////////////////////////////////////////////////
+	// a bit of a hack for now - kick the server and tell it to echo all state to new connections
+	// later propagate new connection event to listeners and they can respond as they wish
+	// may want to better evaluate the forward casting strategy
+	///////////////////////////////////////////////////////////////////////////////////////
 
-	// manually pass a message to network upstream to force server side database to re-send us all its state for now (since we are not making server side db for now)
 	{
-		uuid:"/myusername/apps/basic001/mynet",
-		urn:"*:/services/db",
+		uuid:"/myusername/apps/basic001/mynet", // send msg to net
+		urn:"*:/services/db", // it will route to db
 		command:"sync",
 	},
 
-]
+];
 
-// - also note net should hold traffic till upstream conn is up then send!
-// - 	// may need to implement a forward mechanic more clearly for sync
 
 
 export { myscene, myavatar, myapp }
